9e2aab4c0ac2622ef595cd1cf7deacc9bb15f990
SarahL <luisarah@gmail.com>
2024-09-02 15:54:43 +0200

    Build a navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react';
-import { useState } from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
 	return (
 		//base:平时时候正常小屏幕. 大屏全显
 		<Grid templateAreas={{ base: `"nav""main"`, lg: `"nav nav""aside main"` }}>
-			<GridItem area='nav' bg='orange'>
-				Nav
+			<GridItem area='nav'>
+				<NavBar />
 			</GridItem>
 			{/* Show only when it large screen (laptop) 1024-1440px */}
 			<Show above='lg'>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { HStack, Image, Text } from '@chakra-ui/react';
+import logo from '../assets/log.png';
+
+const NavBar = () => {
+	return (
+		//Horizontal 水平线叠堆
+		<HStack>
+			<Image src={logo} boxSize='60px' />
+			<Text>NavBar</Text>
+		</HStack>
+	);
+};
+
+export default NavBar;

7427ecadef5ca142fdb37592f14bd9525b4cbbf3
SarahL <luisarah@gmail.com>
2024-09-03 16:55:59 +0200

    Show a spinner while fetching the genres.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,13 +1,4 @@
-import {
-	Card,
-	CardBody,
-	CardFooter,
-	CardHeader,
-	Heading,
-	HStack,
-	Image,
-	Text,
-} from '@chakra-ui/react';
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 import { Game } from '../services/game-service';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';

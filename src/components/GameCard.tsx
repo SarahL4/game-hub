eb3a4af7bad231a2811eda758354b7e519cc0064
SarahL <luisarah@gmail.com>
2024-09-03 11:00:30 +0200

    Displaying platform icons.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,8 +5,10 @@ import {
 	CardHeader,
 	Heading,
 	Image,
+	Text,
 } from '@chakra-ui/react';
 import { Game } from '../services/game-service';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
 	game: Game;
@@ -22,6 +24,11 @@ const GameCard = ({ game }: Props) => {
 				<Image src={game.background_image} />
 				<CardBody>
 					<Heading fontSize='2xl'>{game.name}</Heading>
+					<PlatformIconList
+						platforms={game.parent_platforms.map((p) => p.platform)}
+					/>
+					{/* {game.parent_platforms.map(({ platform }) => (
+						<Text>{platform.name}</Text> */}
 				</CardBody>
 				{/* <CardFooter></CardFooter> */}
 			</Card>
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,45 @@
+import {
+	FaWindows,
+	FaPlaystation,
+	FaXbox,
+	FaApple,
+	FaLinux,
+	FaAndroid,
+} from 'react-icons/fa'; //https://react-icons.github.io/react-icons/search/#q=faplaystation
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { Platform } from './../services/game-service';
+import { HStack, Icon } from '@chakra-ui/react';
+import { IconType } from 'react-icons';
+
+interface Props {
+	platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+	//object
+	const iconMap: { [key: string]: IconType } = {
+		//name. Playstation
+		//Slug:playstation
+		pc: FaWindows,
+		playstation: FaPlaystation,
+		xbox: FaXbox,
+		nintendo: SiNintendo,
+		mac: FaApple,
+		linux: FaLinux,
+		ios: MdPhoneIphone,
+		android: FaAndroid,
+		web: BsGlobe,
+	};
+	return (
+		<HStack marginY={1}>
+			{/* //marginY 卡片长并 */}
+			{platforms.map((platform) => (
+				<Icon as={iconMap[platform.slug]} key={platform.id} color='gray.500' />
+				// <Text key={platform.id}>{platform.name}</Text>
+			))}
+		</HStack>
+	);
+};
+export default PlatformIconList;

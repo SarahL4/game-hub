62d8ba8409fa5503e19013915dc535f91de61504
SarahL <luisarah@gmail.com>
2024-09-05 11:10:13 +0200

    37-Refactor GameGrid.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from '@chakra-ui/react';
+import { SimpleGrid, Text } from '@chakra-ui/react';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
@@ -14,28 +14,29 @@ const GameGrid = ({ gameQuery }: Props) => {
 	//->apiClient.get<T>('/games)-apiClient axios create{baseURL+'/games'}
 	//use hook: useGames(), and return data(games,error...)
 	// const { data, error, isLoading } = useData<Game>('/games');
-	const { data, isLoading } = useGames(gameQuery);
+	const { data, isLoading, error } = useGames(gameQuery);
 	const skeletons = [1, 2, 3, 4, 5, 6];
+
+	if (error) return <Text>{error}</Text>;
+
 	return (
-		<>
-			<SimpleGrid
-				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-				spacing={6}
-				padding='10px'
-			>
-				{isLoading &&
-					skeletons.map((s) => (
-						<GameCardContainer key={s}>
-							<GameCardSkeleton />
-						</GameCardContainer>
-					))}
-				{data.map((game) => (
-					<GameCardContainer key={game.id}>
-						<GameCard game={game} />
+		<SimpleGrid
+			columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+			spacing={6}
+			padding='10px'
+		>
+			{isLoading &&
+				skeletons.map((s) => (
+					<GameCardContainer key={s}>
+						<GameCardSkeleton />
 					</GameCardContainer>
 				))}
-			</SimpleGrid>
-		</>
+			{data.map((game) => (
+				<GameCardContainer key={game.id}>
+					<GameCard game={game} />
+				</GameCardContainer>
+			))}
+		</SimpleGrid>
 	);
 };
 

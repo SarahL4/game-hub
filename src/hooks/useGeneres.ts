11d9708019c668bfb93be8b4746caf1b422ba872
SarahL <luisarah@gmail.com>
2024-10-02 14:25:29 +0200

    React 2_install QueryClient, queryDevtools.

---
+++ src/hooks/useGeneres.ts
@@ -0,0 +1,6 @@
+import genres from '../assets/data/genres';
+
+// const useGenres = () => ({ data: genres, isLoading: false, error: null });
+const useGenres = () => useQuery;
+
+export default useGenres;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import theme from './theme';
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<ChakraProvider theme={theme}>
 			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+				<ReactQueryDevtools />
+			</QueryClientProvider>
 		</ChakraProvider>
 	</StrictMode>
 );

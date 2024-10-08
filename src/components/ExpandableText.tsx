fe79c34bd6afb450fd24236bc55bdaa6bd91e7cf
SarahL <luisarah@gmail.com>
2024-10-08 10:59:12 +0200

    4_17-Build expandable Text. Read More, Show less.

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,31 @@
+import { Button, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+
+interface Props {
+	children: string;
+}
+
+const ExpandableText = ({ children }: Props) => {
+	const [expanded, setExpanded] = useState(false); //简单local state不用存到太空飞船store上.
+	const limit = 300;
+	if (!children) return null;
+	if (children.length <= limit) return <Text>{children}</Text>;
+
+	const summary = expanded ? children : children.substring(0, limit) + '...';
+	return (
+		<Text>
+			{summary}
+			<Button
+				marginLeft={1}
+				size='xs'
+				fontWeight='bold'
+				colorScheme='yellow'
+				onClick={() => setExpanded(!expanded)}
+			>
+				{expanded ? 'Show Less' : 'Read More'}
+			</Button>
+		</Text>
+	);
+};
+
+export default ExpandableText;
